9c9afabe293c1351f6a5361992f09271fdd5db57
Endy1381 <119288346+Endy1381@users.noreply.github.com>
2023-09-02 05:11:04 -0700

    Install bootstrap

--- google-replica/src/App.jsx
+++ google-replica/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import 'google'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div className='header'>
+
+      </div>
     </>
   )
 }
